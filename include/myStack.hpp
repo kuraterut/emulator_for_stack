fb7bf447fafb91ab2388312027063e75d78c7ae4
kuraterut <kurylilia@gmail.com>
2024-02-19 19:43:48 +0300

    create myStack class

---
+++ include/myStack.hpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+template<typename DataType>
+class myStack{
+	private:
+		DataType data;
+		class myStack *next;
+	
+	public:
+		//Конструкторы и деструкторы:
+		myStack() = default;
+}
